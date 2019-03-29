a4fdd859aae41e7d871ee7b3d858b46107bfb7c5
pimgeek <wudi.biz@gmail.com>
2019-03-29 22:46:05 +0800

    增加一个新的 class 变量 dump 方法

--- python/utils.py
+++ python/utils.py
@@ -6,3 +6,10 @@ def class_var_dump(a_class):
 	class_vars = vars(a_class)
 	dump_str = "\n".join("%s: [ %s ]" % item for item in class_vars.items() if not item[0].startswith('__'))
 	return dump_str
+
+def class_var_dump2(a_class):
+	"""对 class variable 进行 dump, 以便调试脚本时检查"""
+	var_names = dir(a_class)
+	user_var_names = [item for item in var_names if not item.startswith('__')]
+	dump_str = "\n".join("%s: [ %s ]" % (item, getattr(a_class, item)) for item in user_var_names)
+	return dump_str
